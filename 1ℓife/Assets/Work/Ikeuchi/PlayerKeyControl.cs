8c8a558adfc3517770488cd1eb6952ccb3c80452
Naoto Fujiwara <asyura1327@yahoo.co.jp>
2015-11-03 23:01:35 +0900

    状態変化する部分を変更

---
+++ 1ℓife/Assets/Work/Ikeuchi/PlayerKeyControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKeyControl : MonoBehaviour {
+
+    [SerializeField]
+    float _moveValue = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, 0.0f, _moveValue);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, 0.0f, -_moveValue);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-_moveValue, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_moveValue, 0.0f, 0.0f);
+        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    GetComponent<Rigidbody>().AddForce(0.0f, 200.0f, 0.0f);
+        //}
+    }
+}
