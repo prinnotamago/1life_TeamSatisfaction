2425d2b6522fce7387557e78097f52fbc6cf248d
kohei1030 <climate_1030_kk@yahoo.co.jp>
2015-11-05 01:37:16 +0900

    Playerの一部修正と水蒸気時のカメラアングル変更

--- 1ℓife/Assets/Work/Sugiyama/CameraControl.cs
+++ 1ℓife/Assets/Work/Sugiyama/CameraControl.cs
@@ -12,10 +12,27 @@ public class CameraControl : MonoBehaviour
     [SerializeField]
     private float _positionZ = -1.0f;
 
-    // Update is called once per frame
+    [SerializeField]
+    private float _airPositionY = -0.5f;
+
+    private Player _fujiwaraPlayer;
+
+    void Start()
+    {
+        _fujiwaraPlayer = _Player.GetComponent<Player>();
+    }
+
     void Update()
     {
-        if(_Player != null)
-        this.transform.position = _Player.transform.position + new Vector3(0, _positionY, _positionZ);
+
+        if (_Player != null)
+        {
+            //水蒸気じゃなければ
+           if(_fujiwaraPlayer._playerMode != Player.PlayerMode.AIR)
+                this.transform.position = _Player.transform.position + new Vector3(0, _positionY, _positionZ);
+           //水蒸気だったら
+           else this.transform.position = _Player.transform.position + new Vector3(0, _airPositionY, _positionZ);
+        }
+        else Debug.Log("プレイヤーオブジェクトをドラッグ＆ドロップしてください");
     }
 }
